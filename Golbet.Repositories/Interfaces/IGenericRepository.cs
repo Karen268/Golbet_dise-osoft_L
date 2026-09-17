df20828bee5842bf6273bd6a8cda61d19d962b63
Karen <tatika1107@gmail.com>
2026-09-16 20:42:08 -0500

    Módulo 3: Capa de repositorios y acceso a datos

---
+++ Golbet.Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using Golbet.Entities.Common;
+// GolBet.Repositories/Interfaces/IGenericRepository.cs
+
+namespace GolBet.Repositories.Interfaces;
+
+/// <summary>
+/// Generic data-access contract for all domain entities.
+/// Specific queries live in entity-specific repositories.
+/// </summary>
+public interface IGenericRepository<T> where T : AuditableEntity
+{
+    // ---- Queries ----
+    //Metodo 1
+    Task<IEnumerable<T>> GetAllAsync(bool includeInactive = false);
+    //Metodo 2
+    Task<T?> GetByIdAsync(int id);
+
+    // ---- Commands ----
+    //Metodo 3
+    Task<T> AddAsync(T entity);
+    //Metodo 4
+    Task UpdateAsync(T entity);
+    //Metodo 5
+    Task DeactivateAsync(int id);   // logical delete: IsActive = false
+}
